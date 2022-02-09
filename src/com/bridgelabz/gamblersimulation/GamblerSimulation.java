f5794ba5a7973ed69e84083642261216f3204756
vaibs <vaibhavmuratkar2@gmail.com>
2022-02-09 12:50:46 +0530

    UC2-Win or lose

--- src/com/bridgelabz/gamblersimulation/GamblerSimulation.java
+++ src/com/bridgelabz/gamblersimulation/GamblerSimulation.java
@@ -4,10 +4,32 @@ public class GamblerSimulation {
     public static void main(String[] args) {
         System.out.println("Welcome to Gambler Simulation");
 
-        //UC1
         int EVERY_DAY_STAKE = 100;
         int EVERY_GAME_BET = 1;
 
-        //
+        System.out.println("Starting amount of stake is "+EVERY_DAY_STAKE);
+        System.out.println("Amount to bet after every play "+EVERY_GAME_BET);
+        int play = (int) Math.floor(Math.random() * 10) % 2;
+        System.out.println("Gamblers play:"+play);
+        int cashInHand = EVERY_DAY_STAKE;
+        switch(play) {
+            case 1:
+                cashInHand = cashInHand + EVERY_GAME_BET;
+                System.out.println("Total current cash gambler has "+cashInHand);
+                break;
+            case 2:
+                cashInHand = cashInHand - EVERY_GAME_BET;
+                System.out.println("Total current cash gambler has "+cashInHand);
+                break;
+            default:
+                System.out.println("default");
+        }
+        if(play == 1) {
+            System.out.println("Player won the first play");
+        }
+        else {
+            System.out.println("Player lost the first play");
+        }
+
     }
 }
