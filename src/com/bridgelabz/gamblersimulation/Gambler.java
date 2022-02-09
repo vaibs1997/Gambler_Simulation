99706e29f59049d33d45d3d29addd1f434bc544a
vaibs <vaibhavmuratkar2@gmail.com>
2022-02-09 12:02:30 +0530

    Initial commit

---
+++ src/com/bridgelabz/gamblersimulation/Gambler.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.gamblersimulation;
+
+public class Gambler {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Gambler Simulation");
+    }
+}
