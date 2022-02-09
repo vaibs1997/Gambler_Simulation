22011ef0bf2cf8f6fd38229bf771daa76836a379
vaibs <vaibhavmuratkar2@gmail.com>
2022-02-09 12:19:07 +0530

    Stake and Bet is Added

--- src/com/bridgelabz/gamblersimulation/GamblerSimulation.java
+++ src/com/bridgelabz/gamblersimulation/GamblerSimulation.java
@@ -1,6 +1,6 @@
 package com.bridgelabz.gamblersimulation;
 
-public class Gambler {
+public class GamblerSimulation {
     public static void main(String[] args) {
         System.out.println("Welcome to Gambler Simulation");
 
